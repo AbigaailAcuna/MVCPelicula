a7efddf4c4b200c8e88827b28af6f6a70788892e
Abigail Acuña <acuaabigail@yahoo.com>
2024-07-12 15:00:54 -0600

    Agregando CRUD de géneros
    Ejercicio 1, primera parte

--- MVCPelicula/Models/Genero.cs
+++ MVCPelicula/Models/Genero.cs
@@ -1,8 +1,14 @@
-﻿namespace MVCPelicula.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCPelicula.Models
 {
     public class Genero
     {
         public int Id { get; set; }
+
+        [StringLength(100)]
+        [Required]
         public string Nombre { get; set; }
 
         public ICollection<Pelicula> Peliculas { get; set; }
