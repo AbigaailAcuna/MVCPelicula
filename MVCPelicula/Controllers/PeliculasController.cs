dc5ee5f90db7154df7443bff736632f33ce01416
Abigail Acuña <acuaabigail@yahoo.com>
2024-07-15 14:24:50 -0600

    Modificaciones para mostrar género
    G#1 Ejercicio #1 parte 2

--- MVCPelicula/Controllers/PeliculasController.cs
+++ MVCPelicula/Controllers/PeliculasController.cs
@@ -47,7 +47,7 @@ namespace MVCPelicula.Controllers
         // GET: Peliculas/Create
         public IActionResult Create()
         {
-            ViewData["GeneroId"] = new SelectList(_context.Generos, "Id", "Id");
+            ViewData["GeneroId"] = new SelectList(_context.Generos, "Id", "Nombre");
             return View();
         }
 
@@ -65,7 +65,7 @@ namespace MVCPelicula.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["GeneroId"] = new SelectList(_context.Generos, "Id", "Id", pelicula.GeneroId);
+            ViewData["GeneroId"] = new SelectList(_context.Generos, "Id", "Nombre", pelicula.GeneroId);
             return View(pelicula);
 
         }
@@ -83,7 +83,7 @@ namespace MVCPelicula.Controllers
             {
                 return NotFound();
             }
-            ViewData["GeneroId"] = new SelectList(_context.Generos, "Id", "Id", pelicula.GeneroId);
+            ViewData["GeneroId"] = new SelectList(_context.Generos, "Id", "Nombre", pelicula.GeneroId);
             return View(pelicula);
         }
 
@@ -120,7 +120,7 @@ namespace MVCPelicula.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["GeneroId"] = new SelectList(_context.Generos, "Id", "Id", pelicula.GeneroId);
+            ViewData["GeneroId"] = new SelectList(_context.Generos, "Id", "Nombre", pelicula.GeneroId);
             return View(pelicula);
         }
 
